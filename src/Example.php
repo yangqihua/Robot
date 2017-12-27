146d50b0721cd710bd6862f466b9a7e7d8973bae
yangqihua <904693433@qq.com>
2017-12-27 16:04:56 +0800

    init

---
+++ src/Example.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app;
+
+use Hanson\Vbot\Foundation\Vbot as Bot;
+use Vbot\Blacklist\Blacklist;
+use Vbot\GuessNumber\GuessNumber;
+use Vbot\HotGirl\HotGirl;
+
+class Example
+{
+    private $config;
+
+    public function __construct($session = null)
+    {
+        $this->config = require_once __DIR__.'/config/config.php';
+
+        if ($session) {
+            $this->config['session'] = $session;
+        }
+    }
+
+    public function run()
+    {
+        $robot = new Bot($this->config);
+
+        $robot->messageHandler->setHandler([MessageHandler::class, 'messageHandler']);
+
+        $robot->messageExtension->load([
+            // some extensions
+//            Blacklist::class,
+//            GuessNumber::class,
+//            HotGirl::class,
+        ]);
+
+        $robot->observer->setQrCodeObserver([Observer::class, 'setQrCodeObserver']);
+
+        $robot->observer->setLoginSuccessObserver([Observer::class, 'setLoginSuccessObserver']);
+
+        $robot->observer->setReLoginSuccessObserver([Observer::class, 'setReLoginSuccessObserver']);
+
+        $robot->observer->setExitObserver([Observer::class, 'setExitObserver']);
+
+        $robot->observer->setFetchContactObserver([Observer::class, 'setFetchContactObserver']);
+
+        $robot->observer->setBeforeMessageObserver([Observer::class, 'setBeforeMessageObserver']);
+
+        $robot->observer->setNeedActivateObserver([Observer::class, 'setNeedActivateObserver']);
+
+        $robot->server->serve();
+    }
+}
---
+++ src/MessageHandler.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app;
+
+use app\handlers\contact\Colleaguegroup;
+use app\handlers\contact\Experiencegroup;
+use app\handlers\contact\Feedbackgroup;
+use app\handlers\contact\Hanson;
+use app\handlers\type\Recalltype;
+use app\handlers\type\Texttype;
+
+
+use Hanson\Vbot\Contact\Friends;
+use Hanson\Vbot\Contact\Groups;
+use Hanson\Vbot\Contact\Members;
+use Hanson\Vbot\Message\Emoticon;
+use Hanson\Vbot\Message\Text;
+use Illuminate\Support\Collection;
+
+class MessageHandler
+{
+    public static function messageHandler(Collection $message)
+    {
+        vbot('console')->log('来自'.$message['from']['UserName'].'的消息，消息类型为：'.$message['type'].'，内容为：'.$message['content'], '自定义消息');
+//        Text::send($message['from']['UserName'], 'Hi! I\'m Vbot');
+        /** @var Friends $friends */
+        $friends = vbot('friends');
+
+        /** @var Members $members */
+        $members = vbot('members');
+
+        /** @var Groups $groups */
+        $groups = vbot('groups');
+
+        Hanson::messageHandler($message, $friends, $groups);
+        ColleagueGroup::messageHandler($message, $friends, $groups);
+        FeedbackGroup::messageHandler($message, $friends, $groups);
+        ExperienceGroup::messageHandler($message, $friends, $groups);
+
+        TextType::messageHandler($message, $friends, $groups);
+        RecallType::messageHandler($message);
+
+        if ($message['type'] === 'new_friend') {
+            Text::send($message['from']['UserName'], '客官，等你很久了！感谢跟 vbot 交朋友，如果可以帮我点个star，谢谢了！https://github.com/HanSon/vbot');
+            $groups->addMember($groups->getUsernameByNickname('Vbot 体验群'), $message['from']['UserName']);
+            Text::send($message['from']['UserName'], '现在拉你进去vbot的测试群，进去后为了避免轰炸记得设置免骚扰哦！如果被不小心踢出群，跟我说声“拉我”我就会拉你进群的了。');
+        }
+
+        if ($message['type'] === 'emoticon' && random_int(0, 1)) {
+            Emoticon::sendRandom($message['from']['UserName']);
+        }
+
+        // @todo
+        if ($message['type'] === 'official') {
+            vbot('console')->log('收到公众号消息:'.$message['title'].$message['description'].
+                $message['app'].$message['url']);
+        }
+
+        if ($message['type'] === 'request_friend') {
+            vbot('console')->log('收到好友申请:'.$message['info']['Content'].$message['avatar']);
+            if (in_array($message['info']['Content'], ['echo', 'print_r', 'var_dump', 'print'])) {
+                $friends->approve($message);
+            }
+        }
+    }
+}
