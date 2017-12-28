f303ec2c4babf43381b320c2bad078cf0252514d
yangqihua <904693433@qq.com>
2017-12-28 21:08:19 +0800

    添加 vbot/tuling 库

--- src/handlers/MainHandler.php
+++ src/handlers/MainHandler.php
@@ -16,7 +16,7 @@ use app\common\Http;
 
 class MainHandler
 {
-    public static $baseUrl = 'http://yangqihua.com/quan/public/index.php/api/robot/';
+    public static $baseUrl = 'http://192.168.1.105/quan/public/index.php/api/robot/';
 
     public static function messageHandler(Collection $message, Friends $friends, Groups $groups)
     {
@@ -26,8 +26,10 @@ class MainHandler
             case 'request_friend':
                 vbot('console')->log('收到好友<' . $message['from']['NickName'] . '>申请，申请内容：' . $message['info']['Content']);
                 $friends->approve($message);
-                $result = Http::get(self::$baseUrl . 'add', ['content' => $message['content'], 'user_id' => $message['from']['UserName']]);
-                vbot('console')->log('好友请求服务器返回<' . \GuzzleHttp\json_encode($result) . '>');
+                $result = Http::get(self::$baseUrl . 'add', ['content' => $message['info']['Content'],
+                    'user_id' => $message['from']['UserName'], 'user_name' => $message['from']['NickName'],
+                    'province' => $message['from']['Province'], 'city' => $message['from']['City'], 'sex' => $message['from']['Sex']]);
+                vbot('console')->log('同意添加好友后请求服务器返回<' . $result . '>');
                 break;
             // 同意添加好友后发送 第一次 文案
             case 'new_friend':
@@ -45,21 +47,34 @@ class MainHandler
     }
 
     // 处理 文案
-    public function handlerText($message)
+    public static function handlerText($message)
     {
         $fromType = $message['fromType'];
-        $content = $message['isAt'] ? $message['pure'] : $message['content'];
+        $content = trim($message['isAt'] ? $message['pure'] : $message['content']);
         $username = $message['from']['UserName'];
-        if ($fromType == 'Friend' || ($fromType == 'Group' && $message['isAt'])) {
-            vbot('console')->log('<' . $message['from']['NickName'] . '>发送消息：' . $content);
+        $isAtInGroup = $fromType == 'Group' && $message['isAt'];
+        if ($fromType == 'Friend' || $isAtInGroup) {
+            if ($isAtInGroup) {
+                $groupName = $message['from']['NickName'] ? $message['from']['NickName'] : '无群名称';
+                vbot('console')->log('<群：' . $groupName . ',成员：' . $message['sender']['NickName'] . '>发送消息：' . $content);
+            } else {
+                vbot('console')->log('<' . $message['from']['NickName'] . '>发送消息：' . $content);
+            }
             switch ($content) {
                 case '帮助':
                     Text::send($message['from']['UserName'], self::getConfig('help'));
                     break;
                 // 其他文案 后台去处理
                 default:
-                    $result = self::request($content, $username);
-                    vbot('console')->log('请求后台返回结果：<' . \GuzzleHttp\json_encode($result) . '>');
+                    if ($isAtInGroup) {
+                        $result = self::request($content, $username, '1');
+                        vbot('console')->log('【群聊@】请求后台返回结果：<' . \GuzzleHttp\json_encode($result,JSON_UNESCAPED_UNICODE) . '>');
+                    } else {
+                        $result = self::request($content, $username, '0');
+                        vbot('console')->log('【私聊】请求后台返回结果：<' . \GuzzleHttp\json_encode($result,JSON_UNESCAPED_UNICODE) . '>');
+                    }
+
+                    // 只有当服务器返回200 的时候才回复消息
                     if ($result['code'] == 200) {
                         Text::send($message['from']['UserName'], $result['data']);
                     }
@@ -68,23 +83,32 @@ class MainHandler
         }
     }
 
-    public static function request($content, $username)
+    public static function request($content, $username, $isGroup = '0')
     {
         $url = self::$baseUrl . 'request';
         $result = Http::get($url,
             [
                 'content' => $content,
-                'user_id' => $username
+                'user_id' => $username,
+                'is_group'=> $isGroup
             ]
         );
-        return $result;
+        if($result){
+            $result = \GuzzleHttp\json_decode($result,true);
+            if(!$result){
+                return ['code'=>500,'data'=>''];
+            }
+            return $result;
+        }else{
+            return ['code'=>500,'data'=>''];
+        }
+
     }
 
     public static function getConfig($key)
     {
         $config = [
-            'first' => '
-一一一一添 加 成 功一一一一
+            'first' => '一一一一添 加 成 功一一一一
 送您2元现金，回复 余额 可查看 
 
 输入宝贝信息，获取领券链接
@@ -95,8 +119,7 @@ class MainHandler
 输入  提现   满10元机器人可以给你发红包
 输入  帮助   可以查看指令',
 
-            'help' => '
-一一一一帮 助一一一一
+            'help' => '一一一一帮 助一一一一
 
 输入宝贝信息，获取领券链接
 返利教学： http://t.cn/RHcBT0x
